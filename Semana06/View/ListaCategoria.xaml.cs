3661e8d2fb26b0266c7c32d776da510fdeae7f44
Sabrina Pomajulca <sabrina.pomajulca@tecsup.edu.pe>
2020-05-05 18:15:15 -0500

    primer commit

---
+++ Semana06/View/ListaCategoria.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Semana06.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Semana06.View
+{
+    /// <summary>
+    /// Lógica de interacción para ListaCategoria.xaml
+    /// </summary>
+    public partial class ListaCategoria : Window
+    {
+        ListaCategoriaViewModel viewModel;
+        public ListaCategoria()
+        {
+            InitializeComponent();
+            viewModel = new ListaCategoriaViewModel();
+            this.DataContext = viewModel;
+        }
+      
+
+        
+    }
+}
---
+++ Semana06/ViewModel/ListaCategoriaViewModel .cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Entity;
+
+namespace Semana06.ViewModel
+{
+    public class ListaCategoriaViewModel : ViewModelBase
+    {
+        public ObservableCollection<Categoria> Categorias { get; set; }
+
+        public ICommand NuevoCommand { set; get; }
+        public ICommand ConsultarCommand { set; get; }
+
+        public ListaCategoriaViewModel()
+        {
+            Categorias = new Model.CategoriaModel().Categorias;
+
+            NuevoCommand = new RelayCommand<Window>(
+                param => Abrir()
+                );
+
+            ConsultarCommand = new RelayCommand<Object>(
+                o => { Categorias = (new Model.CategoriaModel()).Categorias; }
+                );
+            void Abrir()
+            {
+                View.ManCategoria window = new View.ManCategoria();
+                window.ShowDialog();
+            }
+        }
+
+    }
+}
